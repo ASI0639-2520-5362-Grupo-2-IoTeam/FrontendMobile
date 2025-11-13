0a2dcb1a33f2fea2779f0875fcdc82939dc2784d
Francisco Hurtado <u202117498@upc.edu.pe>
2025-11-12 20:37:28 -0500

    feat: add community view

---
+++ plant_care/lib/community/presentation/views/community_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../widgets/community_feed_item.dart';
+import '../widgets/expert_item.dart';
+
+class CommunityView extends StatelessWidget {
+  const CommunityView({super.key});
+
+
